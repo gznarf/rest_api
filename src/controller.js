273a1ee567b2f7df49efc5556c0f9675ce1cca4e
gznarf <franz.zalazar@gmail.com>
2023-10-24 21:51:58 -0300

    Handled error with try catch

--- src/controller.js
+++ src/controller.js
@@ -28,33 +28,56 @@ class PersonaController{
 
 class LibroController{
     async getAll(req, res){
-        const [result] = await pool.query('SELECT * FROM libros');
-        res.json(result);
+        try {
+            const [result] = await pool.query('SELECT * FROM libros');
+            res.json(result);
+        } catch (error) {
+            return res.status(500).json({ message: 'Algo salió mal'})
+        }
     }
 
     async getOne(req, res){
+       try {
         const libro = req.params.id;
         const [result] = await pool.query(`SELECT * FROM libros WHERE id=(?)`, [libro]);
         if (result.length <= 0) return res.status(404).json({ message : 'Libro no encontrado'})
         res.json(result[0]);
+       } catch (error) {
+        return res.status(500).json({ message: 'No se encuentra el libro buscado'})
+       }
     }
 
     async add(req, res){
+       try {
         const libro = req.body;
         const [result] = await pool.query(`INSERT INTO Libros(nombre, autor, categoria, año_publicacion, isbn) VALUES(?,?,?,?,?)`, [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.isbn]);
         res.json({"ID Insertado": result.insertId});
+       } catch (error) {
+        return res.status(500).json({ message: 'No se puede agregar el libro'})
+       }
     }
 
     async delete(req, res){
+        try {
         const libro = req.body;
         const [result] = await pool.query(`DELETE FROM Libros WHERE isbn=(?)`, [libro.isbn]);
+        if(result.affectedRows <= 0) return res.status(404).json({message: "Libro no encontrado"})
         res.json({"Registro eliminado": result.affectedRows});
+        } catch (error) {
+            return res.status(500).json({ message: 'No se puede eliminar el libro'})
+        }
     }
 
     async update(req, res){
+        try {
         const libro = req.body;
         const [result] = await pool.query(`UPDATE Libros SET nombre=(?), autor=(?), categoria=(?), año_publicacion=(?), isbn=(?) WHERE id=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.isbn, libro.id]);
+        if(result.affectedRows === 0) return res.status(404).json({message: "Libro no econtrado"})
         res.json({"Registro actualizado": result.changedRows});
+        } catch (error) {
+            return res.status(500).json({ message: 'No se puede actualizar el libro'})
+        }
+        
     }
 }
 
